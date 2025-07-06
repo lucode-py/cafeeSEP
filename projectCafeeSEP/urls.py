b95120bba24ff58eb87354d05d9a3433b1afab7a
lucas <98044419+lucode-py@users.noreply.github.com>
2025-07-06 20:30:33 +0200

    test fix right

--- projectCafeeSEP/urls.py
+++ projectCafeeSEP/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
